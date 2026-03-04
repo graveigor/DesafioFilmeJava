f743b5fb4ca503c7e038fd3e6be451e7e395fb5c
Igor Grave Teixeira <igorgraveteixeira@gmail.com>
2026-03-03 23:50:26 -0300

    method creation

--- src/Movie.java
+++ src/Movie.java
@@ -2,5 +2,13 @@ public class Movie {
     String name;
     int yearOfRelease;
     boolean includedInThePlan;
+    double rating;
+    int totalRatings;
+    int durationInMinutes;
+
+    void displayTechnicalSheet(){
+        System.out.println(name);
+        System.out.println(yearOfRelease);
+    }
 
 }
